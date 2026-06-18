b267132754a8c6f6fa45031c1eeefdcc4ecdd97b
Bilali <bilalidjanfarou@gmail.com>
2026-06-18 06:37:43 +0100

    Django REST Framework Library API

--- library_api/books/models.py
+++ library_api/books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_year = models.IntegerField()
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
