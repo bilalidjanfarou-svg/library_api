f26596f5cad52d3019eeaff646feec63c7025e40
Bilali <bilalidjanfarou@gmail.com>
2026-06-18 21:52:21 +0100

    Add pagination to Book API

--- library_api/library_api/settings.py
+++ library_api/library_api/settings.py
@@ -53,7 +53,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3,
 }
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
